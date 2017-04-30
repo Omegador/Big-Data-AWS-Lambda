c1cadbac8b09c1779185cb2a9a0642721964663f
Omegador <vladislav@nevada.unr.edu>
2017-04-30 16:46:36 -0700

    Uploaded initial files
    With AWS help from Brian Smith and some JS from Max Wiegant

---
+++ Airbnb/index.js
@@ -0,0 +1,94 @@
+var request = require("request");
+var util = require('util'); // todo - remove this
+
+
+//
+// @about - gathers AirBnB listings from a specified zip code
+// @param zip - the zip code to query
+// @param cb - callback function
+// @return error - return error if there was one
+// @return data - return listings if no error
+//
+function gatherListings(zip, cb) {
+	var options = {};
+ 	options.url = "https://www.airbnb.com/api/v2/explore_tabs?version=1.1.0&_format=for_explore_search_web&items_per_grid=60000&fetch_filters=true&supports_for_you_v3=true&timezone_offset=-420&auto_ib=true&tab_id=home_tab&allow_override%5B%5D=&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&currency=USD&locale=en";
+	options.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'};
+	var data;
+
+	// add zip to querystring parameter
+	options.url += "&location=" + zip;
+
+	// DEBUG
+	// console.log(options);
+
+	// make the request to airbnb api
+	request(options, function(err, response, body) {
+  		if(err)	cb(err, null);
+
+  		// crawl through json in body and return the list of listings
+		data = JSON.parse(body);
+		cb(null, data.explore_tabs[0].sections[0].listings);
+  	});
+}
+
+
+function generateScores() {
+  // TODO
+}
+
+
+// TODO - create one general function that both the export and Manual run will call to start the program
+
+
+/* called by Lambda */
+exports.handler = (event, context, callback) => {
+    
+
+	/* TODO - Implement. Model after the code from Manual Run */
+
+
+    callback(null, 'Hello from Lambda');
+};
+
+
+/* Manual Run */
+// only run if 3rd argument is 'manual'
+if(process.argv[2] == "manual") {
+	var output = {};
+	console.log("INFO - running in manual mode.");
+
+	gatherListings(89502, function(err, listings) {
+		if(err) console.log("ERROR: %s", err);
+		else {
+
+			// create each listing object
+			listings.forEach(function(listing) {
+				// construct listing object
+				output.id = listing.listing.id;
+				output.name = listing.listing.name;
+				output.beds = listing.listing.beds;
+				output.pricePerNight = listing.pricing_quote.rate.amount;
+				output.location = {
+					lat: listing.listing.lat,
+					long: listing.listing.lng
+				};
+
+				console.log("Output: %s", util.inspect(output, {showHidden: false, depth: null}))
+
+				// DEBUG 
+				// console.log("Listing id: %s", listing.listing.id);
+				// console.log("Listing name: %s", listing.listing.name);
+				// console.log("Number of beds: %s", listing.listing.beds);
+				// console.log("Price per night: $%s", listing.pricing_quote.rate.amount);
+				// console.log("Location: %s Lat, %s Long", listing.listing.lat, listing.listing.lng);
+				// console.log("---------------------------------------------");
+			});
+		}
+	});
+
+}
+
+
+
+
+
