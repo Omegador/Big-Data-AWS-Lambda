5569525e3ff38d98dc561b93b45518d4bc1f6b81
Max Wiegant <mwiegant@bombora.com>
2017-05-15 18:26:05 -0700

    latest, so far

--- ScoreListing/index.js
+++ ScoreListing/index.js
@@ -1,6 +1,12 @@
 var request = require("request");
 var util = require('util');
 
+// range from which to get Zillow listings
+const LONGITUDE_RANGE = 5000000;
+const LATITUDE_RANGE = 500000;
+const ZILLOW_LISTINGS_DESIRED = 10;
+
+//
 // @about - gathers Zillow listings in a small area, centered at the provided latitude/longitude
 // @param lat - the latitude at the center of area
 // @param long - the longitude at the center of area
@@ -8,8 +14,7 @@ var util = require('util');
 // @return error - return error if there was one
 // @return data - return listings if no error
 //
-function gatherZillowListings(lat, long, cb) 
-{
+function gatherZillowListings(lat, long, cb) {
 	var options = {};
  	options.url = "https://www.zillow.com/search/GetResults.htm?spt=homes&status=001000&lt=000000&ht=111001&pr=,500000&days=12m&mp=,&bd=0%2C&ba=0%2C&sf=,&lot=0%2C&yr=,&singlestory=0&hoa=0%2C&pho=0&pets=0&parking=0&laundry=0&income-restricted=0&pnd=0&red=0&zso=0&ds=all&pmf=1&pf=1&sch=100111&zoom=15&p=1&sort=globalrelevanceex&search=maplist&rid=13478&rt=6&listright=true&isMapSearch=true&zoom=15";
 
@@ -17,13 +22,12 @@ function gatherZillowListings(lat, long, cb)
 	var topleftLong, topleftLat, botRightLong, botRightLat, rect;
 	var data;
 
-	/// get coordinates for area to query
-	// zillow API wants long of -119.79434821622124 represented as -119794348, 
-	// so I multiply by 10^6 and then parseInt() to get rid of remaining floating value
-	topleftLong = parseInt( (long - 0.5) * 1000000 );
-	topleftLat = parseInt( (lat - 0.5) * 1000000 );
-	botRightLong = parseInt( (long + 0.5) * 1000000 );
-	botRightLat = parseInt( (lat + 0.5) * 1000000 );
+	// long and lat have been represented as XXYYYYYY instead of as XX.YYYYYY,
+	// so when I subtract 500000, this is essentially 0.5 latitude
+	topleftLong = long - LONGITUDE_RANGE;
+	topleftLat = lat - LATITUDE_RANGE;
+	botRightLong = long + LONGITUDE_RANGE;
+	botRightLat = lat + LATITUDE_RANGE;
 
 	// construct querystring parameter
 	rect = topleftLong + "," + topleftLat + "," + botRightLong + "," + botRightLat;
@@ -32,7 +36,7 @@ function gatherZillowListings(lat, long, cb)
 	options.url += "&rect=" + rect;
 
 	// DEBUG
-	// console.log(URL: options.url);
+	// console.log("URL:" + options.url);
 
 	// make the request to api
 	request(options, function(err, response, body) {
@@ -41,9 +45,10 @@ function gatherZillowListings(lat, long, cb)
   		// crawl through json in body and return the list of listings
 		data = JSON.parse(body);
 
-		console.log("listing: %s", data.map.properties[0]);
-
-		cb(null, data.map.properties);
+		if(data.map.properties.length > ZILLOW_LISTINGS_DESIRED)
+			cb(null, data.map.properties.splice(0, ZILLOW_LISTINGS_DESIRED));
+		else
+			cb(null, data.map.properties);
   	});
 }
 
@@ -55,16 +60,22 @@ function gatherZillowListings(lat, long, cb)
 //
 function extractDataFromListings(_listings) {
 	var listings = [];
+	var price;
 
-	_listings.forEach(function(listing) {
+	_listings.forEach(function(listing) {	
+		
 		var obj = {};
-		obj.price = listing[8][0];
+		price = listing[3].replace("$", "").replace("K", "");
+		obj.price = parseInt(price) * 1000;
 		obj.sqft = listing[8][3];
 		obj.lat = listing[1];
 		obj.long = listing[2];
 
 		listings.push(obj);
 	});
+
+	// DEBUG
+	// console.log("DEBUG: listings[0]: %s", JSON.stringify(listings[0]));
 
 	return listings;
 }
@@ -77,19 +88,26 @@ function extractDataFromListings(_listings) {
 // @param long - longitude of AirBnB listing
 //
 function calculateMetaStats(listings, lat, long) {
+	var latDifference;
+	var longDifference;
+	var distanceSquared;
 
 	listings.forEach(function(listing) {
-		// calculate score
+		// calculate Zillow score for Zillow listing
+		listing.score = parseInt(listing.price / listing.sqft);
 
-		// calculate distance
+		// calculate distance from this Zillow listing to the airbnb listing
+		latDifference = parseInt( Math.abs(listing.lat - lat) / 10000 );
+		longDifference = parseInt( Math.abs(listing.long - long) / 100000 );
+		distanceSquared = Math.pow(latDifference, 2) + Math.pow(longDifference, 2);
+		listing.distance = parseInt(distanceSquared / 100);
+
+		// DEBUG
+		// console.log("DEBUG - calculateMetaStats, listing.price: " + listing.price);
+		// console.log("DEBUG - calculateMetaStats, listing.sqft: " + listing.sqft);
+		// console.log("DEBUG - calculateMetaStats, listing.score: " + listing.score);
+		// console.log("DEBUG - calculateMetaStats, listing.distance: " + listing.distance);
 	})
-
-	// calculate score
-
-
-	// TODO
-
-
 
 }
 
@@ -105,6 +123,12 @@ function calculateMetaStats(listings, lat, long) {
 function generateZScore(lat, long, cb) {
 	var listings, bins, zScore;
 
+	lat = parseInt(lat * 1000000);
+	long = parseInt(long * 1000000);
+
+	// DEBUG
+	// console.log("DEBUG - generateZScore, lat: " + lat);
+	// console.log("DEBUG - generateZScore, long: " + long);
 
 	// gather Zillow listings
 	gatherZillowListings(lat, long, function(err, listings) {
@@ -143,27 +167,17 @@ exports.handler = (event, context, callback) => {
 // only run if 3rd argument is 'manual'
 if(process.argv[2] == "manual") {
 	var event = {
-		lat: 39.52273506458795,
+		lat: 39.522735,
 		lng: -119.78078512264162
 	};
 	var output = {};
 
 	console.log("INFO - running in manual mode.");
 
-	// todo - remove
-	// gatherZillowListings(39.52273506458795, -119.78078512264162, function(err, listings) {
-	// 	if(err) console.log("ERROR: %s", err);
-	// 	else {
-
-	// 		// console.log("data: %s", util.inspect(listings, { showHidden: true, depth: null }));
-	// 		console.log("number of listings found: %d", listings.length);
-	// 	}
-	// });
-
-	// generate Z Score
+	// generate Z Score, which is the output score produced by this program
 	generateZScore(event.lat, event.lng, function(err, data) {
 
-		console.log("number of listings found: %d", data.length);
+		console.log("DEBUG - manual_run, number of listings found: %d", data.length);
 
 		// data.forEach(function(_data) {
 		// 	console.log("listing: %s", util.inspect(_data, {showHidden: false, depth: null}));
